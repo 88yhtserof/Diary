8679319556d4f85fe0a7d29d1951542cc228f594
LimYunhwi <88yhtserof@naver.com>
2022-02-28 00:13:31 +0900

    일기 앱 만들기:  기능 (1)
    1. 전체적으로 아울렛 변수, 액션함수 생성
    2. 일기 추가 화면에서 내용 텍스트 필드의 테두리 UI를 코드로 수정

--- Diary/WriteDiaryViewController.swift
+++ Diary/WriteDiaryViewController.swift
@@ -8,8 +8,26 @@
 import UIKit
 
 class WriteDiaryViewController: UIViewController {
-
+    @IBOutlet weak var titleTextField: UITextField!
+    @IBOutlet weak var contentsTextView: UITextView!
+    @IBOutlet weak var dateTextField: UITextField!
+    @IBOutlet weak var confirmButton: UIBarButtonItem!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        self.configureContentsTextView()
+    }
+    
+    //내용 텍스트필드의 UI 수정하기
+    private func configureContentsTextView(){
+        let borderColor = UIColor(red: 220/255, green: 220/255, blue: 220/255, alpha: 1.0)//alpha 값이 0.0에 가까울 수록 투명도가 올라간다.(0.0 ~ 1.0)
+        //RGB값에는 alpha값과 동일하게 0.0에서 1.0 사이의 값을 넣어주어야 한다. 따라서 설정하려는 RGB값을 255로 나눠주어야 한다.
+        self.contentsTextView.layer.borderColor = borderColor.cgColor//layer관련해 색을 설정할 때는 UIColor가 아닌 cgColor로 설정해야 한다.그래서 UIColor에 있는 cgColor프로퍼티를 사용한다.
+        self.contentsTextView.layer.borderWidth = 0.5 //테두리 두께
+        self.contentsTextView.layer.cornerRadius = 5.0
+    }
+    
+    @IBAction func tapConfirmButton(_ sender: UIBarButtonItem) {
     }
 }
